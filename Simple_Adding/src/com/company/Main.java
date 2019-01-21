e7b05c5246179d34802d4a81ff97e80dfd323472
Reka <rekapap28@gmail.com>
2019-01-21 21:58:14 +0000

    adds Simple Adding implementation

--- Simple_Adding/src/com/company/Main.java
+++ Simple_Adding/src/com/company/Main.java
@@ -1,8 +1,33 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
-    public static void main(String[] args) {
-	// write your code here
+
+public class Main {
+    private static int SimpleAdding(int num) {
+        // if (num <= 0) return num;
+
+        int startNumber = 1;
+        int sum = 0;
+
+        while (startNumber <= num)
+        {
+            sum += startNumber;
+            startNumber++;
+        }
+
+        return sum;
+
     }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        s.nextLine();
+        System.out.print(SimpleAdding(number));
+    }
+
 }
